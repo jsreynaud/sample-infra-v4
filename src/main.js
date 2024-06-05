5eef4d879c1a66d9fbbdaf0ec955e69e23bc15d7
Jean-Samuel REYNAUD <jeansamuel.reynaud@qowisio.com>
2024-06-05 08:55:23 +0200

    TP1

--- src/main.js
+++ src/main.js
@@ -1,10 +1,47 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+// Adding a middleware to handle json data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 /**
  * Define the main function for mail module
  */
 function run() {
-    // TODO
-    console.log("TODO");
+    app.get('/', (req, res) => {
+        res.send('Hello World!')
+    })
+
+    app.get('/login', (req, res) => {
+        res.send('You want to login ?')
+    })
+
+    app.get('/data', (req, res) => {
+        res.send('You want access data ?')
+    })
+
+    app.post('/data', (req, res) => {
+        if (req.body.username && req.body.email) {
+            const key1 = req.body.username;
+            const key2 = req.body.email;
+            // Reformat the data for testing...
+            res.send({
+                'key1': key1,
+                'key2': key2
+            });
+        } else {
+            // Fallback
+            console.log(req.body);
+            res.send("Invalid data");
+        }
+    });
+
+
+    app.listen(port, () => {
+        console.log(`Server listening on port ${port}`)
+    })
 }
 
 // Export this function outside
